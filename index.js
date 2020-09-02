49c9ee7e2b09f76109d379bce953a9a01cb0fb2b
Mateo Diaz Bolatti <mateolinobolatti@gmail.com>
2020-09-02 13:30:13 -0300

    hello-database2

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const mongoose = require('mongoose');
 // agregamos esta linea
 const cors     = require('cors');
 
-const app = express()
-
-// ahora tambien importamos mongoose
-const express  = require('express');
-const mongoose = require('mongoose');
-
 // importamos el router que creamos para la api
 const router = require('./routes/api/user');
 
@@ -18,7 +12,7 @@ const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
 
 const app = express();
-
+app.use(cors());
 // conexion a la base de datos
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
@@ -35,5 +29,5 @@ app.use('/api', router);
 // el server escucha todo
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-app.use(cors());
+
 });
